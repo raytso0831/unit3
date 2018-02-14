f099f1ecdd5662828782f8d997f01dedb82865d5
raytso0831 <35492837+raytso0831@users.noreply.github.com>
2018-02-14 08:33:34 -0500

    Updated from Brython Server: 2/14/2018 8:33:33 AM

--- loopdemo.py
+++ loopdemo.py
@@ -4,40 +4,61 @@
 
 """
 #I love comp sci 5 times
-for i in range(1,6):
-    print(' I <3 Computer Science')
+for i in range(0,5):
+    print('I <3 Computer Science')
 """
-i=1
-while i<=10
-print(i)
-"""
-#while loop version
-i=0
-while i<5:
-    print('I<3 Computer Science')
-    i=i+1#update step
 
 """
+#while loop version
+i = 0
+while i<5:
+    print('I <3 Computer Science')
+    i = i+1 #update step
+"""
+
+"""
+#Counts from 1 to 10
+for i in range(1,11):
+    print(i)
+"""
+
+"""
+i = 1
+while i<= 10:
+    print(i)
+    i += 1 #means add one to i
+"""
+
+""""
 #Count from 27 to 43 by 2s
 for i in range(27,44,2):
     print(i)
 """
 
 """
+i = 27
+while i <= 43:
+    print(i)
+    i += 2
+"""
+
+"""
 #add up the numbers from 1 to 5
-total=0
+total = 0
 for i in range(1,6):
-    total=total+i
+    total = total + i
+print(total)
+"""
+
+total = 0
+i = 1
+while i <= 5:
+    total = total + i
+    i += 1
 print(total)
 
-I=27
-whilei<=43
-    print(i)
-    
-    
-total=)
-i=1
-while i<=5:
-    total=toal+i
-    i+1=1
-print(total)
+
+
+
+
+
