b5636cfc2e394c5b6eb1eb553c3dc6fe1403ae7b
raytso0831 <35492837+raytso0831@users.noreply.github.com>
2018-02-14 09:40:26 -0500

    Updated from Brython Server: 2/14/2018 9:40:25 AM

--- favorites.py
+++ favorites.py
@@ -1,1 +1,8 @@
+#Ray Tso
+#2/14/18
+#loopsdemo.py
 
+fav_word=input('What is your favorite word')
+fav_num=int(input('What is your favorite number'))
+for i in range(0,fav_num):
+    print(fav_word)
