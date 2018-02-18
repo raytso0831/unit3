9af59c1aa5d9b993ed5f64bdd384dfd4c10895a7
raytso0831 <35492837+raytso0831@users.noreply.github.com>
2018-02-18 13:45:44 -0500

    Updated from Brython Server: 2/18/2018 1:45:43 PM

--- unitConverterUPDATED.py
+++ unitConverterUPDATED.py
@@ -8,23 +8,20 @@ print('choice_3=  Liters to Gallons')
 print('choice_4= Celsius to Fahrenheit')
 
 while True:
-    choice = (input('Choose a number from 1-4 or type quit: '))
+    choice = (input('Choose a number or type quit: '))
     if choice == '1':
         kilom = float(input('Enter number of Kilometers: '))
-        print(kilom, 'kilometers is',kilom*.621371,'miles')
+        print(kilom, 'kilometers is',kilom*.621,'miles')
     elif choice == '2':
-        kilo = float(input('Enter a number of Kilograms: '))
-        print(kilo, 'kilograms is',kilo*2.204,'pounds')
+        kilog = float(input('Enter a number of Kilograms: '))
+        print(kilog, 'kilograms is',kilog*2.204,'pounds')
     elif choice == '3':
         liter = float(input('Enter a number of Liters: '))
-        print(liter, 'liters is',liter*.264,'gallons')
-    elif choice==4:
-        celsius = float(input('Enter degrees in Celcius: '))
-        print(celsius, 'degrees Celsius is',)
+        print(liter, 'liters is',l*.264,'gallons')
     elif choice == 'quit':
         break
     else:
-        print('ERROR!!!')
-        break
-
+        celsius = float(input('Enter degrees in Celsius: '))
+        print(celsius, 'degrees Celsius is',celsius*1.8+32,'degrees Fahrenheit')
+    
     
