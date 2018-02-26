7e3f5de1f56ccd698be889062e5ffb5122f22165
raytso0831 <35492837+raytso0831@users.noreply.github.com>
2018-02-26 08:48:52 -0500

    Updated from Brython Server: 2/26/2018 8:48:52 AM

--- perfectNumber.py
+++ perfectNumber.py
@@ -1,3 +1,11 @@
 #Ray Tso
 #2/18/18
 #perfectNumber.py
+
+num=int(input('Enter a number:'))
+for i in range(1,num):
+    if num%i==0:
+        print(i)
+    
+    if i==num:
+        print('This number is a a perfect number')
