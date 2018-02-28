d1d98f2dde1dd84dd4606130a228c8a82dc029da
raytso0831 <35492837+raytso0831@users.noreply.github.com>
2018-02-28 08:22:34 -0500

    Updated from Brython Server: 2/28/2018 8:22:32 AM

--- betterAdditiongame.py
+++ betterAdditiongame.py
@@ -8,4 +8,10 @@ numCorrect=0
 while numCorrect<5:
     num1=randint(-10,10)
     num2=randint(-10,10)
-    input('What is'+ str(num1)+'+'+ str(num2)+'?')
+    answer= int(input('What is'+ str (num1)+'+'+ str(num2)+ '?'))
+    if answer==num1+num2:
+        numCorrect+=1
+    else:
+        print('Wrong')
+        print('The answer was', num1+num2)
+print('You win ! You are an addition champ!')
