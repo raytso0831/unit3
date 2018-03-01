283b342a137ccd64e5f34ebf953344a1792fe4a3
raytso0831 <35492837+raytso0831@users.noreply.github.com>
2018-03-01 10:50:55 -0500

    Updated from Brython Server: 3/1/2018 10:50:54 AM

--- changeComputer.py
+++ changeComputer.py
@@ -1,3 +1,17 @@
 #Ray Tso
-#2/26/18
+#3/1/18
 #changeComputer.py
+
+change=int(input('Enter the amount of change:'))
+
+quarter=25
+dime=10
+nickel=5
+penny=1
+
+if change-quarter>25:
+    change-quarter
+
+elif change-quarter<25:
+    
+    
