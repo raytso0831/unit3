3b8cfdaa9f8a47cd66d70a3b924ccf33d3c64dc9
raytso0831 <35492837+raytso0831@users.noreply.github.com>
2018-03-01 08:22:35 -0500

    Updated from Brython Server: 3/1/2018 8:22:34 AM

--- warmup9.py
+++ warmup9.py
@@ -1,3 +1,10 @@
 #Ray Tso
 #3/1/18
-#warmup.py
+#warmup9.py
+
+word = input('Enter a word: ')
+for ch in word:
+    if ch == 'a' or ch == 'A' or ch == 'e' or ch == 'E' or ch == 'i' or ch == 'I' or ch == 'o' or ch == 'O' or ch == 'u' or ch == 'U':
+        print(ch.upper())
+    else:
+        print(ch)
