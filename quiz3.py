32d6c687a0f0b7f1aef99903441c26aaa4fc6d2d
raytso0831 <35492837+raytso0831@users.noreply.github.com>
2018-03-05 08:47:36 -0500

    Updated from Brython Server: 3/5/2018 8:47:35 AM

--- quiz3.py
+++ quiz3.py
@@ -2,26 +2,29 @@
 #3/5/18
 #quiz3.py
 
-"""
+
 for i in range(50,14,-4):
     print(i)
-"""
 
-"""
+
+
 i = -15
 while i <=-9 :
     print(i)
     i += 1
-"""
-"""
-for i in range(-100,100):
-    if  i%2==0:
-        print(i)
-"""
-"""
+    
+num=1
+total=0
+while num<100000:
+    if num%2==0
+        total+=num
+    num+=1
+print(total)
+        
+
 while True:
     text=input('Say anything:')
     if text=='alpaca':
         break
     print(text)
-"""
+
