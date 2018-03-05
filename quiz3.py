1dab62697dcb0a8f0b41c12c5fe5d7d040086ffb
raytso0831 <35492837+raytso0831@users.noreply.github.com>
2018-03-05 08:32:10 -0500

    Updated from Brython Server: 3/5/2018 8:32:09 AM

--- quiz3.py
+++ quiz3.py
@@ -2,4 +2,26 @@
 #3/5/18
 #quiz3.py
 
+"""
+for i in range(50,14,-4):
+    print(i)
+"""
 
+"""
+i = -15
+while i <=-9 :
+    print(i)
+    i += 1
+"""
+"""
+for i in range(-100,100):
+    if  i%2==0:
+        print(i)
+"""
+"""
+while True:
+    text=input('Say anything:')
+    if text=='alpaca':
+        break
+    print(text)
+"""
